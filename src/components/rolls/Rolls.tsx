7cee70c3232acc27a2b7b32dfaa4f1fa8ae3e8e6
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-01-15 17:30:23 -0500

    FIX: Add Space Between .Roll

--- src/components/rolls/Rolls.tsx
+++ src/components/rolls/Rolls.tsx
@@ -4,7 +4,7 @@ import Roll from './Roll';
 
 function Rolls() {
   return (
-    <div className='Rolls grid grid-cols-1 gap-2 px-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
+    <div className='Rolls grid grid-cols-1 gap-8 px-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
       {Array.from({ length: rolls }, (_, i) => i).map((roll) => (
         <Roll roll={roll.toString()} key={roll} />
       ))}
